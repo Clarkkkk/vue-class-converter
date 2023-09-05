01817e5a5ad17a2bfac5bd9366da6e793be01125
Aaron_Zhou <clark1729@outlook.com>
2023-09-06 00:27:35 +0800

    test: add tests for jsx case

--- src/utils/tests/arrayClassToString.test.ts
+++ src/utils/tests/arrayClassToString.test.ts
@@ -2,9 +2,23 @@ import { expect, describe, test } from 'vitest'
 import { arrayClassToString } from '../arrayClassToString'
 
 describe('arrayClassToString', () => {
-    test('array', () => {
-        expect(arrayClassToString(':class="[\'flex\', \'items-center\']"')).toMatchInlineSnapshot('"class=\\"flex items-center\\""')
+    test('vue sfc array', () => {
+        expect(arrayClassToString(":class=\"['flex', 'items-center']\"")).toMatchInlineSnapshot(
+            '"class=\\"flex items-center\\""'
+        )
 
-        expect(arrayClassToString('<div :class="[\'flex\', \'items-center\']">')).toMatchInlineSnapshot('"<div class=\\"flex items-center\\">"')
+        expect(
+            arrayClassToString("<div :class=\"['flex', 'items-center']\">")
+        ).toMatchInlineSnapshot('"<div class=\\"flex items-center\\">"')
+    })
+
+    test('jsx array', () => {
+        expect(arrayClassToString("class={['flex', 'items-center']}")).toMatchInlineSnapshot(
+            '"class=\\"flex items-center\\""'
+        )
+
+        expect(arrayClassToString("<div class={['flex', 'items-center']}>")).toMatchInlineSnapshot(
+            '"<div class=\\"flex items-center\\">"'
+        )
     })
 })
--- src/utils/tests/stringClassToArray.test.ts
+++ src/utils/tests/stringClassToArray.test.ts
@@ -2,9 +2,19 @@ import { describe, expect, test } from 'vitest'
 import { stringClassToArray } from '../stringClassToArray'
 
 describe('stringClassToArray', () => {
-    test('string', () => {
-        expect(stringClassToArray('class="flex items-center"')).toMatchInlineSnapshot('":class=\\"[\'flex\', \'items-center\']\\""')
+    test('vue sfc string', () => {
+        expect(stringClassToArray('class="flex items-center"', 'vue')).toMatchInlineSnapshot(
+            '":class=\\"[\'flex\', \'items-center\']\\""'
+        )
 
-        expect(stringClassToArray('<div class="flex items-center">')).toMatchInlineSnapshot('"<div :class=\\"[\'flex\', \'items-center\']\\">"')
+        expect(stringClassToArray('<div class="flex items-center">', 'vue')).toMatchInlineSnapshot(
+            '"<div :class=\\"[\'flex\', \'items-center\']\\">"'
+        )
+    })
+
+    test('jsx string', () => {
+        expect(stringClassToArray('class="flex items-center"', 'jsx')).toMatchInlineSnapshot('"class={[\'flex\', \'items-center\']}"')
+
+        expect(stringClassToArray('<div class="flex items-center">', 'jsx')).toMatchInlineSnapshot('"<div class={[\'flex\', \'items-center\']}>"')
     })
 })
